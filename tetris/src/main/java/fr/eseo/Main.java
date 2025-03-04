aa48183eaddee44fd06b94a42ba97c958ea129fe
floalzaix <floalzfencing@gmail.com>
2025-03-04 09:02:06 +0100

    Ajout des dossier et crétion de l'enum Couleur

--- tetris/src/main/java/fr/eseo/Main.java
+++
@@ -1,7 +0,0 @@
-package fr.eseo;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ tetris/src/main/java/fr/eseo/e3/poo/projet/blox/Tetris.java
@@ -0,0 +1,7 @@
+package fr.eseo.e3.poo.projet.blox;
+
+public class Tetris {
+    public static void main(String[] args) {
+
+    }
+}
